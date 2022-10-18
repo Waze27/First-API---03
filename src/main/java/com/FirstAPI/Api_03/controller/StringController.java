7fb19e06e9a976ca74b3691c6ff88a40da273c30
Mirko Di Cristina <mirko7726@gmail.com>
2022-10-18 17:24:15 +0200

    Fix error

--- src/main/java/com/FirstAPI/Api_03/controller/StringController.java
+++ src/main/java/com/FirstAPI/Api_03/controller/StringController.java
@@ -2,15 +2,17 @@ package com.FirstAPI.Api_03.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotBlank;
 
 
 @RestController
 @RequestMapping("/strings")
 public class StringController {
 
-    @GetMapping(value = "/{prime}/{second}")
-    public String concat(@NotBlank(message = "mandatory") @PathVariable String prime, @PathVariable String second){
-        return prime.concat(second);
+    @GetMapping
+    public String concat(@RequestParam String prime, @RequestParam(required = false) String second) {
+        if(second == null) {
+            second = "";
+        }
+        return prime.concat(" " + second);
     }
 }
